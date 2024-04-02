e9c226390e911dd7afaa09d39f2088592f6c4fc9
Hunkon <153524469+BobReed24@users.noreply.github.com>
2024-04-01 21:51:03 -0500

    Update lockdown-check.js

--- lockdown-check.js
+++ lockdown-check.js
@@ -1,16 +1,17 @@
-// Function to check lockdown status
-function checkLockdown() {
-    fetch('lockdown-status.txt') // Fetch the lockdown status from the server
-        .then(response => response.text()) // Convert the response to text
-        .then(data => {
-            if (data.trim() === 'true') { // Check if lockdown is enabled
-                window.location.href = 'lockdown.html'; // Redirect to lockdown page if lockdown is enabled
-            }
-        })
-        .catch(error => {
-            console.error('Error checking lockdown status:', error); // Log any errors
-        });
-}
-
 // Check lockdown status every 5 seconds
 setInterval(checkLockdown, 5000);
+
+function checkLockdown() {
+    fetch('lockdown-status.txt')
+    .then(response => response.text())
+    .then(data => {
+        if (data.trim() === 'true') {
+            window.location.href = 'lockdown.html';
+        } else {
+            console.log('Lockdown does not apply on this page.');
+        }
+    })
+    .catch(error => {
+        console.error('Error checking lockdown status:', error);
+    });
+}
