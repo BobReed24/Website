4e77def1e3435159a5bc11b9d3cdbc6acf3e04ee
Hunkon <153524469+BobReed24@users.noreply.github.com>
2024-04-02 16:50:33 -0500

    Update flappybird-script.js

--- flappybird-script.js
+++ flappybird-script.js
@@ -8,91 +8,63 @@ document.addEventListener('DOMContentLoaded', function() {
     // Getting reference to the bird element
     let bird = document.querySelector('.bird');
 
-    // Getting bird element properties
-    let bird_props = bird.getBoundingClientRect();
-    let background =
-        document.querySelector('.background')
-        .getBoundingClientRect();
-
     // Getting reference to the score element
-    let score_val =
-        document.querySelector('.score_val');
-    let message =
-        document.querySelector('.message');
-    let score_title =
-        document.querySelector('.score_title');
+    let scoreVal = document.querySelector('.score_val');
+    let message = document.querySelector('.message');
+    let scoreTitle = document.querySelector('.score_title');
 
     // Setting initial game state to start
-    let game_state = 'Start';
+    let gameState = 'Start';
 
     // Add an eventlistener for key presses
-    document.addEventListener('keydown', (e) => {
-
-        // Start the game if enter key is pressed
-        if (e.key == 'Enter' &&
-            game_state != 'Play') {
-            document.querySelectorAll('.pipe_sprite')
-                .forEach((e) => {
-                    e.remove();
-                });
-            bird.style.top = '40vh';
-            game_state = 'Play';
-            message.innerHTML = '';
-            score_title.innerHTML = 'Score : ';
-            score_val.innerHTML = '0';
+    document.addEventListener('keydown', function(e) {
+        // Start the game if Enter key is pressed
+        if (e.key === 'Enter' && gameState !== 'Play') {
+            resetGame();
             play();
         }
     });
 
+    function resetGame() {
+        document.querySelectorAll('.pipe_sprite').forEach(function(element) {
+            element.remove();
+        });
+        bird.style.top = '40vh';
+        gameState = 'Play';
+        message.innerHTML = '';
+        scoreTitle.innerHTML = 'Score : ';
+        scoreVal.innerHTML = '0';
+    }
+
     function play() {
         function move() {
-
             // Detect if game has ended
-            if (game_state != 'Play') return;
+            if (gameState !== 'Play') return;
 
             // Getting reference to all the pipe elements
-            let pipe_sprite = document.querySelectorAll('.pipe_sprite');
-            pipe_sprite.forEach((element) => {
+            let pipeSprites = document.querySelectorAll('.pipe_sprite');
 
-                let pipe_sprite_props = element.getBoundingClientRect();
-                bird_props = bird.getBoundingClientRect();
+            pipeSprites.forEach(function(element) {
+                let pipeSpriteProps = element.getBoundingClientRect();
+                let birdProps = bird.getBoundingClientRect();
 
-                // Delete the pipes if they have moved out
-                // of the screen hence saving memory
-                if (pipe_sprite_props.right <= 0) {
+                // Delete the pipes if they have moved out of the screen hence saving memory
+                if (pipeSpriteProps.right <= 0) {
                     element.remove();
                 } else {
                     // Collision detection with bird and pipes
-                    if (
-                        bird_props.left < pipe_sprite_props.left +
-                        pipe_sprite_props.width &&
-                        bird_props.left +
-                        bird_props.width > pipe_sprite_props.left &&
-                        bird_props.top < pipe_sprite_props.top +
-                        pipe_sprite_props.height &&
-                        bird_props.top +
-                        bird_props.height > pipe_sprite_props.top
-                    ) {
-
-                        // Change game state and end the game
-                        // if collision occurs
-                        game_state = 'End';
-                        message.innerHTML = 'Press Enter To Restart';
-                        message.style.left = '28vw';
+                    if (birdProps.left < pipeSpriteProps.left + pipeSpriteProps.width &&
+                        birdProps.left + birdProps.width > pipeSpriteProps.left &&
+                        birdProps.top < pipeSpriteProps.top + pipeSpriteProps.height &&
+                        birdProps.top + birdProps.height > pipeSpriteProps.top) {
+                        endGame();
                         return;
                     } else {
-                        // Increase the score if player
-                        // has the successfully dodged the
-                        if (
-                            pipe_sprite_props.right < bird_props.left &&
-                            pipe_sprite_props.right +
-                            move_speed >= bird_props.left &&
-                            element.increase_score == '1'
-                        ) {
-                            score_val.innerHTML = +score_val.innerHTML + 1;
+                        // Increase the score if player has successfully dodged the pipes
+                        if (pipeSpriteProps.right < birdProps.left && element.increaseScore === '1') {
+                            scoreVal.innerHTML = +scoreVal.innerHTML + 1;
                         }
-                        element.style.left =
-                            pipe_sprite_props.left - move_speed + 'px';
+                        element.style.left = pipeSpriteProps.left - move_speed + 'px';
                     }
                 }
             });
@@ -101,68 +73,66 @@ document.addEventListener('DOMContentLoaded', function() {
         }
         requestAnimationFrame(move);
 
-        let bird_dy = 0;
+        let birdDY = 0;
 
-        function apply_gravity() {
-            if (game_state != 'Play') return;
-            bird_dy = bird_dy + gravity;
-            document.addEventListener('keydown', (e) => {
-                if (e.key == 'ArrowUp' || e.key == ' ') {
-                    bird_dy = -7.6;
+        function applyGravity() {
+            if (gameState !== 'Play') return;
+            birdDY = birdDY + gravity;
+
+            document.addEventListener('keydown', function(e) {
+                if (e.key === 'ArrowUp' || e.key === ' ') {
+                    birdDY = -7.6;
                 }
             });
 
-            // Collision detection with bird and
-            // window top and bottom
-
-            if (bird_props.top <= 0 ||
-                bird_props.bottom >= background.bottom) {
-                game_state = 'End';
-                message.innerHTML = 'Press Enter To Restart';
-                message.style.left = '28vw';
+            // Collision detection with bird and window top and bottom
+            let birdProps = bird.getBoundingClientRect();
+            if (birdProps.top <= 0 || birdProps.bottom >= window.innerHeight) {
+                endGame();
                 return;
             }
-            bird.style.top = bird_props.top + bird_dy + 'px';
-            bird_props = bird.getBoundingClientRect();
-            requestAnimationFrame(apply_gravity);
+
+            bird.style.top = birdProps.top + birdDY + 'px';
+            requestAnimationFrame(applyGravity);
         }
-        requestAnimationFrame(apply_gravity);
+        requestAnimationFrame(applyGravity);
 
-        let pipe_seperation = 0;
+        let pipeSeparation = 0;
+        let pipeGap = 35; // Constant value for the gap between two pipes
 
-        // Constant value for the gap between two pipes
-        let pipe_gap = 35;
+        function createPipe() {
+            if (gameState !== 'Play') return;
 
-        function create_pipe() {
-            if (game_state != 'Play') return;
-
-            // Create another set of pipes
-            // if distance between two pipe has exceeded
-            // a predefined value
-            if (pipe_seperation > 115) {
-                pipe_seperation = 0
+            // Create another set of pipes if distance between two pipe has exceeded a predefined value
+            if (pipeSeparation > 115) {
+                pipeSeparation = 0;
 
                 // Calculate random position of pipes on y axis
-                let pipe_posi = Math.floor(Math.random() * 43) + 8;
-                let pipe_sprite_inv = document.createElement('div');
-                pipe_sprite_inv.className = 'pipe_sprite';
-                pipe_sprite_inv.style.top = pipe_posi - 70 + 'vh';
-                pipe_sprite_inv.style.left = '100vw';
+                let pipePos = Math.floor(Math.random() * 43) + 8;
+                let pipeSpriteInv = createPipeSprite(pipePos - 70);
+                let pipeSprite = createPipeSprite(pipePos + pipeGap);
 
-                // Append the created pipe element in DOM
-                document.body.appendChild(pipe_sprite_inv);
-                let pipe_sprite = document.createElement('div');
-                pipe_sprite.className = 'pipe_sprite';
-                pipe_sprite.style.top = pipe_posi + pipe_gap + 'vh';
-                pipe_sprite.style.left = '100vw';
-                pipe_sprite.increase_score = '1';
-
-                // Append the created pipe element in DOM
-                document.body.appendChild(pipe_sprite);
+                document.body.appendChild(pipeSpriteInv);
+                document.body.appendChild(pipeSprite);
             }
-            pipe_seperation++;
-            requestAnimationFrame(create_pipe);
+            pipeSeparation++;
+            requestAnimationFrame(createPipe);
         }
-        requestAnimationFrame(create_pipe);
+        requestAnimationFrame(createPipe);
+    }
+
+    function createPipeSprite(topPosition) {
+        let pipeSprite = document.createElement('div');
+        pipeSprite.className = 'pipe_sprite';
+        pipeSprite.style.top = topPosition + 'vh';
+        pipeSprite.style.left = '100vw';
+        pipeSprite.increaseScore = '1';
+        return pipeSprite;
+    }
+
+    function endGame() {
+        gameState = 'End';
+        message.innerHTML = 'Press Enter To Restart';
+        message.style.left = '28vw';
     }
 });
